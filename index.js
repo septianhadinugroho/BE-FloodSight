2409f1904579f1ea8b0a0ebcaaff901e553c9525
Sifa Mutiasya <sifamutiasya14@gmail.com>
2025-05-09 23:06:28 +0700

    api user

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 require('dotenv').config();
 
-const Hapi = require('hapi');
+const Hapi = require('@hapi/hapi');
 const mongoose = require('mongoose');
 const User = require("./models/user.model");
-
-// koneksi ke MongoDB
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('Connected to MongoDB Atlas'))
-.catch((err) => console.error('MongoDB connection error:', err));
+const userRoutes = require('./routes/user.routes');
 
 const init = async () => {
-    const server = Hapi.server({
-        port: process.env.PORT || 3000,
-        host: 'localhost'
-    });
+    try {
+       await mongoose.connect(process.env.MONGO_URI);
+        console.log('Connected to MongoDB Atlas');
 
-    // contoh route
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            return 'Hello Hapi with MongoDB Atlas!';
-        }
-    });
+        const server = Hapi.server({
+            port: process.env.PORT || 3000,
+            host: 'localhost'
+        });
 
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+        server.route({
+            method: 'GET',
+            path: '/',
+            handler: (request, h) => {
+                return 'Hello Hapi with MongoDB Atlas!';
+            }
+        });
+
+        server.route(userRoutes);
+
+        await server.start();
+        console.log('Server running on %s', server.info.uri);
+
+    } catch (err) {
+        console.error('Failed to start server:', err.message);
+        process.exit(1);
+    }
 };
 
 init();
